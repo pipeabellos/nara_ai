f95493a416f6794d96bcfeb54d898a46f1eb8153
Felipe Abello <pipe@helloguru.io>
2023-01-21 18:12:04 -0800

    add example.js

---
+++ example.js
@@ -0,0 +1,17 @@
+export default {
+    accounts: [
+        {
+            email: 'pipe@helloguru.io',
+            password: 'LzYY07g@0tUQ##EP$QpvEjAIGF',
+            isGoogleLogin: true
+            // Any other options that `ChatGPTAPIBrowser` supports...
+        }
+        // Add more accounts as needed...
+    ],
+    // The port the server will run on (optional, defaults to 3000)
+    port: 3000,
+    // Your NopeCHA API key. This will be applied to all accounts but can be overridden on a per-account basis.
+    nopechaKey: 'sub_1MSWAfCRwBwvt6ptBnVYr4xI',
+    // Your 2Captcha API key. This will be applied to all accounts but can be overridden on a per-account basis.
+    twoCaptchaKey: '319fcec781542cd8d35230de876ace00',
+}
